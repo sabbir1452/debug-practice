1d626a2d93dd4919a6f1df250e298e406346c8ee
sabbir1452 <sabbirrahman1452@gmail.com>
2024-02-12 09:49:37 -0800

    akdfjo

---
+++ practice.js
@@ -0,0 +1,21 @@
+/* 
+Revise JavaScript
+1. 6 Core concepts: variable, conditionals, array, function, loop (for,while, for of, for in), object
+2. arrow function
+3. template string
+4. spread operator
+5. map, filter, forEach, find
+6. destructuring, optional chaining
+7. ternary operator
+8. JSON (parse, stringify), fetch, async await, error
+9. localStorage, seesionStorage
+10. function parameter
+11. dev tool
+12. &&, ||, ++, --, !, !!, ==, ===, null, undefined
+
+*/
+
+/* 
+Debugging:
+
+*/
